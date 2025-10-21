d1378aba7a52b29e075a8aee42970cf4e3af27b4
ybabhulkar43 <ybabhulkar43@gmail.com>
2025-10-21 07:55:21 +0530

    This is the updated changes in Python files....

--- dict1.py
+++ dict1.py
@@ -9,3 +9,15 @@ print('Dictionary is:',dict1)
 #-------------------------------------------------------------------
 t1=tuple(dict1.keys())
 print('Tuple is:',t1)
+#-----------------------------------------------------------------
+dict1={l1:l2 for l1,l2 in zip(l1,l2)}
+print('The Dictionary is:',dict1)
+
+print('The Dict Items Are:')
+for x in dict1:
+    print(x,'--->',dict1[x])
+
+print('The Dict Items Are(using for loop with value x and Y):')
+for x,y in dict1.items():
+    print(x,'--->',y)
+
