8c3ff5522ff41aaf91a0d6f32bd42688e072f5c6
ybabhulkar43 <ybabhulkar43@gmail.com>
2025-10-22 11:20:07 +0530

    soda and water

--- hungry.py
+++ hungry.py
@@ -1,6 +1,13 @@
-input=input('Are you hungry..')
-if input=='yes':
-     print('Eat samosa...')
-     print('Eat burger...')
+user_input = input('Are you hungry? ')
+
+if user_input.lower() == 'yes':
+     print('Eat samosa..')
+     print('Eat burger..')
+     print('Eat pizza..')
 else:
-     print('Do your homework...')
+     thirsty = input('Are you thirsty? ')
+     if thirsty.lower() == 'yes':
+          print('Drink water..')
+          print('Drink soda..')
+     else:
+          print('Okay, have a nice day!')
