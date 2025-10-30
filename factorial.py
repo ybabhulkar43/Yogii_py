d2b2880d3f00a114d4068f053d9ca4f8ba1fce60
ybabhulkar43 <ybabhulkar43@gmail.com>
2025-10-30 22:03:22 +0530

    This is the revert -n commit with commit_id

--- factorial.py
+++
@@ -1,8 +0,0 @@
-def fact(num):
-    if num==1:
-        return 1
-    else:
-        return num*fact(num-1)
-
-res=fact(4)
-print(res)
--- higher.py
+++ higher.py
@@ -4,4 +4,3 @@ even_lst=[i for i in lst1 if i%2==0]
 print(even_lst)
 div_b=[i for i in lst1 if i%5==0]
 print(div_b)
-print('I am the best i the world..')
